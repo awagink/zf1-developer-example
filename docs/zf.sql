0e962364d9df12a492a9d24392325aa3a8d39d13
Matt Simpson <matt@silentweb.ca>
2012-10-02 11:57:00 -0400

    Updated database schema to include locale stuff and articles.

--- docs/zf.sql
+++ docs/zf.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.1.54)
 # Database: zf
-# Generation Time: 2012-10-01 13:57:16 +0000
+# Generation Time: 2012-10-02 15:55:26 +0000
 # ************************************************************
 
 
@@ -20,6 +20,70 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
+# Dump of table article_tags
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `article_tags`;
+
+CREATE TABLE `article_tags` (
+  `article_id` int(11) unsigned NOT NULL,
+  `tag_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`article_id`,`tag_id`),
+  KEY `tag_id` (`tag_id`),
+  CONSTRAINT `article_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `article_tags_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table articles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `articles`;
+
+CREATE TABLE `articles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `article` longtext NOT NULL,
+  `published_date` bigint(64) unsigned NOT NULL DEFAULT '0',
+  `open_date` bigint(64) unsigned NOT NULL DEFAULT '0',
+  `close_date` bigint(64) unsigned NOT NULL DEFAULT '0',
+  `user_id` int(11) unsigned DEFAULT NULL,
+  `locale_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `locale_id` (`locale_id`),
+  CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `articles_ibfk_2` FOREIGN KEY (`locale_id`) REFERENCES `locales` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table locales
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `locales`;
+
+CREATE TABLE `locales` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `locale` varchar(11) NOT NULL DEFAULT '',
+  `title` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `locale` (`locale`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `locales` WRITE;
+/*!40000 ALTER TABLE `locales` DISABLE KEYS */;
+
+INSERT INTO `locales` (`id`, `locale`, `title`)
+VALUES
+	(1,'en','English (US)'),
+	(2,'fr','Français');
+
+/*!40000 ALTER TABLE `locales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
 # Dump of table products
 # ------------------------------------------------------------
 
@@ -27,11 +91,11 @@ DROP TABLE IF EXISTS `products`;
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL DEFAULT '',
   `description` text NOT NULL,
   `order` int(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
@@ -43,6 +107,62 @@ VALUES
 
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+# Dump of table roles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `role` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table tags
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tags`;
+
+CREATE TABLE `tags` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `tag` varchar(128) NOT NULL DEFAULT '',
+  `count` int(11) unsigned DEFAULT '0',
+  `locale_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tag` (`tag`),
+  KEY `locale_id` (`locale_id`),
+  CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`locale_id`) REFERENCES `locales` (`id`) ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(32) DEFAULT NULL,
+  `lastname` varchar(32) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `salt` varchar(64) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `locale_id` int(11) unsigned DEFAULT NULL,
+  `role_id` int(11) unsigned DEFAULT NULL,
+  `updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `locale_id` (`locale_id`),
+  CONSTRAINT `users_ibfk_2` FOREIGN KEY (`locale_id`) REFERENCES `locales` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 
